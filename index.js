9209c9e5b694761da4f49687b046ad87a5a18154
KrisLamb21 <1763669@cegepmontpetit.ca>
2025-05-02 09:14:50 -0400

    brancheDEV

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('je suis fatiguer....\n');
+    res.end('je suis fatiguer......\n');
 });
 
 server.listen(3000, () => {
